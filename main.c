a2128d273367ee8d24fc52e2ef0a70bb6e77d1e8
Zack Kollar <rallokkcaz@gmail.com>
2021-06-23 17:26:38 -0700

    :hammer: Actually process some audio, fuck yeah

--- main.c
+++ main.c
@@ -10,10 +10,16 @@
 #include <stdbool.h>
 #include <string.h>
 #include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
 
 // ====================================================================================
 
 #include <portsf.h>
+
+// ====================================================================================
+
+size_t FRAME_BUFFER_SIZE = 128; 
 
 // ====================================================================================
 // Declarations
@@ -30,9 +36,10 @@ struct audio_file {
 };
 
 // Audio file structure methods
-struct audio_file *audio_file_new               (const char *infile           );
-void               audio_file_info              (struct audio_file *audio_file);
-static inline bool audio_file_valid_sample_type (PSF_PROPS  *properties       );
+struct audio_file *audio_file_open   (const char *infile                                   );
+struct audio_file *audio_file_create (struct audio_file *audio_file_in, const char *outfile);
+void               audio_file_info   (struct audio_file *audio_file                        );
+static inline int  audio_file_free   (struct audio_file *audio_file                        );
 
 // Local methods
 float get_max_gain   (struct audio_file *audio_file);
@@ -49,10 +56,14 @@ void  process_signal (struct audio_file *audio_file);
  */
 int main(int argc, char *argv[]) {
     // PortSF initialization
-    psf_init();
+    if(psf_init()) {
+        printf("Failed to start portsf...");
+        return 1;
+    }
 
     // Values to be set by arguments
-    char *infile = NULL;
+    bool debug = false;
+    char *infile = NULL, *outfile = NULL;
     float gain = 1.0f;
 
     // ==================================
@@ -67,47 +78,99 @@ int main(int argc, char *argv[]) {
 
     // Parse command line arguments with ancient MAGIC
     char flag; // Current flag for the option parsing
-    while ((flag = getopt(argc, argv, "f:g:")) != -1) {
+    while ((flag = getopt(argc, argv, "i:o:g:d")) != -1) {
         switch(flag) {
-            case 'f':
+            case 'i':
                 infile = optarg;
-                break; // NOTE TO SELF: Forgot this and thought I was losing my mind
+                break;
+            case 'o':
+                outfile = optarg;
+                break;
+            case 'd':
+                debug = true;
+                break;
             case 'g':
                 if(optarg) { // How to handle an optional field postfix'd with :
                     gain = atof(optarg);
                 }
                 break;
             case '?':
+                printf("Invalid flag: %s\n", optarg);
                 usage();
                 return 1;
         }
     }
     
-    // Is a file specified?
     if(!infile) {
-        printf("No file to load specified, use the -f flag\n");
+        printf("No file to load specified, use the -i flag\n");
+        usage();
+        return 1;
+    }
+    if(!outfile) {
+        printf("No file to create specified, use the -o flag\n");
         usage();
         return 1;
     }
 
     // Open the file from the CLI args
-    struct audio_file *audio_file = audio_file_new(infile);
+    struct audio_file *audio_file = audio_file_open(infile);
     if(audio_file == NULL) {
         return 1;
     }
 
     // Print audio file info
-    audio_file_info(audio_file);
-    printf("Adjusted Gain: %f\n", gain);
+    if(debug) {
+        audio_file_info(audio_file);
+        printf("Adjusted Gain: %f\n", gain);
+        printf("Output file: %s\n", outfile);
+    }
+
+    // Open a new file
+    struct audio_file *audio_file_out = audio_file_create(audio_file, outfile);
+    if(audio_file_out == NULL) {
+        return 1;
+    }
+
+
+    // Prepare to process the audio data
+    float *frame = malloc(audio_file->properties.chans * sizeof(float));
+    long total_frames = 0;
+
+    // Read each frame
+    while(psf_sndReadFloatFrames(audio_file->file, frame, 1) == 1) {
+        total_frames++;
+
+        // For each channel of the frame
+        for(int i = 0; i < audio_file->properties.chans; i++) {
+            // TODO: Process here
+            frame[i] *= gain;
+        }
+
+        // Write our frames to the output file
+        if(psf_sndWriteFloatFrames(audio_file_out->file, frame, 1) != 1) {
+            printf("Failed to write to outfile %s\n", outfile);
+            break;
+        }
+    }
+
+    free(frame);
+
+    // Close the files
+    if(audio_file_free(audio_file) != 0) {
+        printf("Failed to close file %s\n", infile);
+        audio_file_free(audio_file_out);
+        return 1;
+    }
+    if(audio_file_free(audio_file_out) != 0) {
+        printf("Failed to close file %s\n", infile);
+        return 1;
+    }
 
     // Shutdown PortSF
     if (psf_finish() != 0) {
         printf("Failed to clean up portsf...");
         return 1;
     }
-
-    // Destroy the pointer to the audio file
-    free(audio_file);
 
     return 0;
 }
@@ -120,20 +183,50 @@ int main(int argc, char *argv[]) {
  * @param infile 
  * @return struct audio_file* 
  */
-struct audio_file *audio_file_new(const char *infile) {
+struct audio_file *audio_file_open(const char *infile) {
     // Create the audio file struct
     struct audio_file *audio_file = malloc(sizeof(struct audio_file));
     if(audio_file == NULL) return NULL;
 
-    // Setup the audio file
+    // Setup the existing audio file
     audio_file->file_name = infile;
     audio_file->file = psf_sndOpen(infile, &audio_file->properties, 0); // Open the sound file
     if(audio_file->file < 0) {
         printf("Cannot open file: %s\n", infile);
+        free(audio_file);
         return NULL;
     }
 
     return audio_file;
+}
+
+struct audio_file *audio_file_create(struct audio_file *audio_file_in, const char *outfile) {
+    // Create the audio file struct
+    struct audio_file *audio_file = malloc(sizeof(struct audio_file));
+    if(audio_file == NULL) return NULL;
+
+    // Setup the new audio file
+    audio_file->file_name = outfile;
+    audio_file->properties = audio_file_in->properties;
+    audio_file->properties.samptype = PSF_SAMP_IEEE_FLOAT;
+    audio_file->properties.format = psf_getFormatExt(outfile);
+
+    if(audio_file->properties.format == PSF_FMT_UNKNOWN) {
+        printf("Unrecognized file format (use .aiff or .wav): %s\n", outfile);
+        goto fail;
+    }
+
+    audio_file->file = psf_sndCreate("./test.wav", &audio_file->properties, 0, 0, PSF_CREATE_RDWR); // Create a sound file
+    if(audio_file->file < 0) {
+        printf("Cannot create file: %s\n", outfile);
+        goto fail;
+    }
+
+    return audio_file;
+
+fail:
+    free(audio_file);
+    return NULL;
 }
 
 /**
@@ -147,10 +240,22 @@ void audio_file_info(struct audio_file *audio_file) {
     printf("Channel Count: %d\n", audio_file->properties.chans);
 }
 
+/**
+ * @brief Free the audio file
+ * 
+ * @param audio_file 
+ */
+static inline int audio_file_free(struct audio_file *audio_file) {
+    int result = psf_sndClose(audio_file->file);
+    free(audio_file);
+    return result;
+}
+
+
 // ====================================================================================
 
 static void usage() {
-    printf("audio-gain -f <FILE_NAME> [-g <GAIN>]\n");
+    printf("audio-gain -f <FILE_NAME> [-g <GAIN> | -d <DEBUG> ]\n");
 }
 
 // ====================================================================================
